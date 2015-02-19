6c3f60ccbad0a389dae6ed332981707bfc66826f
Paul Chun <me@paulchun.com>
2015-02-19 16:21:29 -0500

    Fix createapp

--- createapp.py
+++ createapp.py
@@ -13,5 +13,5 @@ if len(sys.argv) <= 1:
 print "Creating %s App..." % sys.argv[1]
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 os.chdir(os.path.join(PROJECT_ROOT, 'django_starter/apps'))
-os.system('python ../../manage.py createapp %s' % sys.argv[1])
+print os.system('python ../../manage.py startapp %s' % sys.argv[1])
 print "App %s created!" % sys.argv[1]
